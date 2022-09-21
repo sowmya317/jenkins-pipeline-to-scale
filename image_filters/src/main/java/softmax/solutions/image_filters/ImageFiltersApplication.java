8dc08056b45daae2c9f5c7dc6993b7655c929bd0
sowmya317 <ubuntu@ip-172-31-80-235.ec2.internal>
2022-09-21 17:56:43 +0000

    image_filters folder added

---
+++ image_filters/src/main/java/softmax/solutions/image_filters/ImageFiltersApplication.java
@@ -0,0 +1,13 @@
+package softmax.solutions.image_filters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageFiltersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageFiltersApplication.class, args);
+	}
+
+}
