4eb836af39838976c6d1b235ac943e4c49af8586
Ubuntu <ubuntu@ip-172-31-80-235.ec2.internal>
2022-09-21 17:49:31 +0000

    image_filters

---
+++ src/test/java/softmax/solutions/image_filters/ImageFiltersApplicationTests.java
@@ -0,0 +1,13 @@
+package softmax.solutions.image_filters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageFiltersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
